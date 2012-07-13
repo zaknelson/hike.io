6b818144a88fc0cd8bd8258101d4e486647ff17c
Zak Nelson <zak.nelson@gmail.com>
2012-07-12 20:49:42 -0700

    Refactoring entry path, and variables to use non-hike specific terms.

--- main.rb
+++ main.rb
@@ -4,24 +4,13 @@ require "sinatra/base"
 require "sinatra/content_for"
 
 class Entry
-	attr_accessor :id, :main_image, :name, :thumbs, :map
+	attr_accessor :id, :name, :pictures, :map
 
 	def initialize(options)
 		@id = options[:id]
-		@main_image = options[:main_image]
 		@name = options[:name]
-		@thumbs = options[:thumbs]
+		@pictures = options[:pictures]
 		@map = options[:map]
-	end
-end
-
-class Thumb
-	attr_accessor :image, :width, :height
-
-	def initialize(options)
-		@image = options[:image]
-		@width = options[:width]
-		@height = options[:height]
 	end
 end
 
@@ -35,20 +24,28 @@ class Map
 	end
 end
 
+class Picture
+	attr_accessor :id
+
+	def initialize(options)
+		@id = options[:id]
+	end
+end
+
 helpers do
 
 	#
 	# TODO real database
 	#
 
-	def thumbs
-		[Thumb.new({:image => "scotchmans-peak-trees-thumb.jpg", :width => 338, :height => 224}), 
-		 Thumb.new({:image => "scotchmans-peak-mountain-goat-thumb.jpg", :width => 338, :height => 224}),
-		 Thumb.new({:image => "scotchmans-peak-dead-tree-thumb.jpg", :width => 338, :height => 509}),
-		 Thumb.new({:image => "scotchmans-peak-goat-view-thumb.jpg", :width => 338, :height => 224}),
-		 Thumb.new({:image => "scotchmans-peak-wildflower-thumb.jpg", :width => 338, :height => 224}),
-		 Thumb.new({:image => "scotchmans-peak-zak-thumb.jpg", :width => 338, :height => 224}),
-		 Thumb.new({:image => "scotchmans-peak-pend-orielle-thumb.jpg", :width => 338, :height => 125})]
+	def pictures
+		[Picture.new({:id => "scotchmans-peak-trees"}),
+		 Picture.new({:id => "scotchmans-peak-mountain-goat"}),
+		 Picture.new({:id => "scotchmans-peak-dead-tree"}),
+		 Picture.new({:id => "scotchmans-peak-goat-view"}),
+		 Picture.new({:id => "scotchmans-peak-wildflower"}),
+		 Picture.new({:id => "scotchmans-peak-zak"}),
+		 Picture.new({:id => "scotchmans-peak-pend-orielle"})]
 	end
 
 	def map
@@ -56,11 +53,10 @@ helpers do
 	end
 
 	def popular_list
-
-		return [Entry.new({:id => "scotchmans-peak", :main_image => "scotchmans-peak.jpg", :name => "Scotchman's Peak", :thumbs => thumbs, :map => map}), 
-    			Entry.new({:id => "king-arthurs-seat", :main_image => "king-arthurs-seat.jpg", :name => "King Arthur's Seat", :thumbs => thumbs, :map => map}),
-    			Entry.new({:id => "north-kaibab-trail", :main_image => "north-kaibab-trail.jpg", :name => "North Kaibab Trail", :thumbs => thumbs, :map => map}),
-    			Entry.new({:id => "mt-kilamanjaro", :main_image => "mt-kilamanjaro.jpg", :name => "Mt. Kilamanjaro", :thumbs => thumbs, :map => map})]
+		return [Entry.new({:id => "scotchmans-peak", :name => "Scotchman's Peak", :pictures => pictures, :map => map}), 
+    			Entry.new({:id => "king-arthurs-seat", :name => "King Arthur's Seat", :pictures => pictures, :map => map}),
+    			Entry.new({:id => "north-kaibab-trail", :name => "North Kaibab Trail", :pictures => pictures, :map => map}),
+    			Entry.new({:id => "mt-kilamanjaro", :name => "Mt. Kilamanjaro", :pictures => pictures, :map => map})]
 	end
 
 	def find_entry id
@@ -82,18 +78,23 @@ before do
 		@public_base_path = "http://assets.hike.io"
 	end
 
-	@hike_images_path = "#{@public_base_path}/hike-images"
+	@entry_img_dir = "#{@public_base_path}/hike-img"
+	@js_dir = "#{@public_base_path}/js"
+	@css_dir = "#{@public_base_path}/css"
+	@img_dir = "#{@public_base_path}/img"
+
 end
 
 get "/" do
 	@title = "hike.io - Beautiful Hikes"
-	@preview_list = popular_list
+	@popular_entry_list = popular_list
 	erb :index
 end
 
-get "/hikes/:hike_id" do
-	@entry = find_entry params[:hike_id]
-	redirect "/" unless @entry
+get "/:entry_id", :provides => 'html' do
+	@entry = find_entry params[:entry_id]
+	puts  params[:entry_id]
+	pass unless @entry
 	@title = @entry.name
 	erb :entry
 end
