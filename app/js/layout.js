31b8c96a2fabeab8d7f585aca620f68dccc7cfed
Zak Nelson <zak.nelson@gmail.com>
2012-09-09 09:55:40 -0700

    Factoring search js into its own file.

--- app/js/layout.js
+++ app/js/layout.js
@@ -14,65 +14,8 @@ _gaq.push(["_trackPageview"]);
 		}
 	};
 
-	var isSearchActive = function() {
-		return $(".search-div").css("display") != "none"
-	}
-
-	var toggleSearchBox = function() {
-		if (isSearchActive()) {
-			if (Modernizr.touch) {
-				$(".search-div").css("display", "none");
-				$(".search-div > input").val("");
-				$(".search-div > input").blur();
-			} else {
-				$(".search-div").animate({
-					marginTop: "-55px"
-				}, 100, function() {
-					$(".search-div").css("display", "none");
-					$(".search-div > input").val("");
-					$(".search-div > input").blur();
-				});
-			}
-		} else {
-			$(".search-div").css("display", "block");
-			if (Modernizr.touch) {
-				$(".search-div").css("margin-top", "0px");
-			} else {
-				$(".search-div").animate({
-					marginTop: "0px"
-				}, 100);
-			}
-			$(".search-div > input").focus();
-		}
-	}
-
-	var setupSearch = function() {
-		$(".header-div-search").click(function(event) {
-			toggleSearchBox();
-			return false;
-		});
-
-		$(".search-div > input").keyup(function(e) {
-			switch (e.keyCode) {
-				case 27: // esc
-				case 13: // return
-					toggleSearchBox();
-					break;
-				default:
-					break;
-			}
-		});
-
-		$("body").click(function(event) {
-			if (isSearchActive() && !$(event.target).closest(".search-div").length) {
-				toggleSearchBox();
-			}
-		});
-	}
-
 	$(document).ready(function() {
 		disableNonTouchFeatures();
-		setupSearch();
 	});
 }
 )();
