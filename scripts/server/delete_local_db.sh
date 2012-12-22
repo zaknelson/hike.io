9b9c72684da8574669b021fac8c06c04fbac5230
Zak Nelson <zak.nelson@gmail.com>
2012-12-22 00:49:33 -0800

    Deleting local db script can now be called from any directory.

--- scripts/server/delete_local_db.sh
+++ scripts/server/delete_local_db.sh
@@ -1,2 +1,5 @@
-psql -q -h localhost -c "DROP DATABASE hikeio"
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+pushd $dir > /dev/null
+psql -q -h localhost -c "DROP DATABASE hikeio" > /dev/null
 rm -f ../../development.db
+popd > /dev/null
