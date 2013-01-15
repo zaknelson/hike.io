19c5a648e2a6c92f51407658ad38dd1d0ff52afa
Zak Nelson <zak.nelson@gmail.com>
2013-01-14 22:50:10 -0800

    Cleanup contenteditable description.

--- client/js/utils/content-editable-utils.js
+++ client/js/utils/content-editable-utils.js
@@ -3,34 +3,21 @@
 	
 	var ContentEditableUtils = function() {
 	};
-	
-	ContentEditableUtils.prototype.replaceSelectedText = function(replacementText) {
-		var range = null;
-		if (window.getSelection) {
-			var selection = window.getSelection();
-			if (selection.rangeCount) {
-				range = selection.getRangeAt(0);
-				range.deleteContents();
-				range.insertNode(document.createTextNode(replacementText));
-				selection.addRange(range);
-				selection.collapseToEnd();
-			}
-		} else if (document.selection && document.selection.createRange) {
-			range = document.selection.createRange();
-			range.text = replacementText;
-		}
-	};
 
 	ContentEditableUtils.prototype.getTextFromContentEditable = function(contentEditable) {
 		var pre = $("<pre/>").html(contentEditable.html());
-		if ($.browser.webkit) {
-			pre.find("div").replaceWith(function() { return "\n" + this.innerHTML; });
-		} else if ($.browser.msie)
-			pre.find("p").replaceWith(function() { return this.innerHTML + "<br>"; });
-		else if ($.browser.mozilla || $.browser.opera || $.browser.msie) {
-			pre.find("br").replaceWith("\n");
-		}
-		return pre.text().trim();
+		pre.find("div").replaceWith(function() { return "\n" + this.innerHTML; });
+		pre.find("p").replaceWith(function() { return this.innerHTML + "<br>"; });
+		pre.find("br").replaceWith("\n");
+		var tempParagraphs = pre.text().trim().split("\n");
+		var resultParagraphs = [];
+		$.each(tempParagraphs, function(index, value) {
+			value = value.trim();
+			if (value !== "") {
+				resultParagraphs.push("<p>" + value + "</p>");
+			}
+		});
+		return resultParagraphs.join("");
 	};
 
 	// Export
