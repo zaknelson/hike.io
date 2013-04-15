dc7c73cf6d3e7ebd2812591a93ce67628ef80614
Zak Nelson <zak.nelson@gmail.com>
2013-04-14 20:36:02 -0700

    Adding save and done implementations for edit page.

--- client/js/controllers/entry-controller.js
+++ client/js/controllers/entry-controller.js
@@ -5,6 +5,7 @@ var EntryController = function($scope, $http, $location, $window) {
 	$scope.hike = null;
 	$scope.isLoaded = false;
 	$scope.isDirty = false;
+	$scope.isSaving = false;
 
 	$http({method: "GET", url: "/api/v1" + $location.path()}).
 		success(function(data, status, headers, config) {
@@ -19,5 +20,23 @@ var EntryController = function($scope, $http, $location, $window) {
 		// Tests whether location is of the form /hikes/my-hike (with no /edit at the end)
 		var regex = /\/hikes\/.*?\/edit/; 
 		return regex.test($location.path());
+	};
+
+	$scope.save = function() {
+		$scope.isSaving = true;
+		$http({method: "PUT", url: "/api/v1/hikes/" + $scope.hike.string_id, data: $scope.hike}).
+			success(function(data, status, headers, config) {
+				$scope.isSaving = false;
+				$scope.isDirty = false;
+			}).
+			error(function(data, status, headers, config) {
+				$scope.isSaving = false;
+				$scope.isDirty = false;
+			});
+	};
+
+
+	$scope.done = function() {
+		$location.path("/hikes/" + $scope.hike.string_id);
 	};
 };
