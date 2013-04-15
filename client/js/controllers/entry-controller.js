60e6114506784a7f5a1c683322e4e5ddd23fa461
Zak Nelson <zak.nelson@gmail.com>
2013-04-14 20:10:40 -0700

    Update isDirty flag when edits have been made.

--- client/js/controllers/entry-controller.js
+++ client/js/controllers/entry-controller.js
@@ -3,13 +3,14 @@ var EntryController = function($scope, $http, $location, $window) {
 
 	$scope.editing = false;
 	$scope.hike = null;
-	$scope.hasLoaded = false;
+	$scope.isLoaded = false;
+	$scope.isDirty = false;
 
 	$http({method: "GET", url: "/api/v1" + $location.path()}).
 		success(function(data, status, headers, config) {
 			$scope.hike = data;
 			$window.document.title = data.name + " - hike.io";
-			$scope.hasLoaded = true;
+			$scope.isLoaded = true;
 		}).
 		error(function(data, status, headers, config) {
 		});
@@ -18,5 +19,5 @@ var EntryController = function($scope, $http, $location, $window) {
 		// Tests whether location is of the form /hikes/my-hike (with no /edit at the end)
 		var regex = /\/hikes\/.*?\/edit/; 
 		return regex.test($location.path());
-	}
+	};
 };
