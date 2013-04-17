dfd5573b63efae63b2f5affacb5096b66dbfeab2
Zak Nelson <zak.nelson@gmail.com>
2013-04-16 19:20:13 -0700

    Remove trailing / from hike getter.

--- client/js/controllers/photo-stream-controller.js
+++ client/js/controllers/photo-stream-controller.js
@@ -1,7 +1,7 @@
 "use strict";
 var PhotoStreamController = function($scope, $http, analytics) {
 
-	$http({method: "GET", url: "/api/v1/hikes/"}).
+	$http({method: "GET", url: "/api/v1/hikes"}).
 		success(function(data, status, headers, config) {
 			$scope.hikes = data;
 		}).
