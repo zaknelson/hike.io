7bfa558f8224bc8ca7c3caf19a1888b80127a1cc
Zak Nelson <zak.nelson@gmail.com>
2013-04-20 12:13:07 -0700

    Cache all hikes api call.

--- client/js/controllers/photo-stream-controller.js
+++ client/js/controllers/photo-stream-controller.js
@@ -1,7 +1,7 @@
 "use strict";
-var PhotoStreamController = function($scope, $http, analytics) {
+var PhotoStreamController = function($scope, $http, analytics, resourceCache) {
 
-	$http({method: "GET", url: "/api/v1/hikes"}).
+	$http({method: "GET", url: "/api/v1/hikes", cache: resourceCache}).
 		success(function(data, status, headers, config) {
 			$scope.hikes = data;
 		}).
@@ -11,4 +11,4 @@ var PhotoStreamController = function($scope, $http, analytics) {
 	$scope.hikes = [];
 };
 
-PhotoStreamController.$inject = ["$scope", "$http", "analytics"];
+PhotoStreamController.$inject = ["$scope", "$http", "analytics", "resourceCache"];
