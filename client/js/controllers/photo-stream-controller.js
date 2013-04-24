949a999172f7a9f729683636ed4e8a66bc417c40
Zak Nelson <zak.nelson@gmail.com>
2013-04-24 00:24:44 -0700

    Enable logging in PhotoStreamController.js.

--- client/js/controllers/photo-stream-controller.js
+++ client/js/controllers/photo-stream-controller.js
@@ -1,13 +1,15 @@
 "use strict";
-var PhotoStreamController = function($scope, $http, analytics, resourceCache) {
+var PhotoStreamController = function($scope, $http, $log, analytics, resourceCache) {
 
 	$http({method: "GET", url: "/api/v1/hikes", cache: resourceCache}).
 		success(function(data, status, headers, config) {
 			$scope.hikes = data;
 		}).
-		error(function(data, status, headers, config) {});
+		error(function(data, status, headers, config) {
+			$log.error(config);
+		});
 
 	$scope.hikes = [];
 };
 
-PhotoStreamController.$inject = ["$scope", "$http", "analytics", "resourceCache"];
+PhotoStreamController.$inject = ["$scope", "$http", "$log", "analytics", "resourceCache"];
