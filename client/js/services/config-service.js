e01b2ce381e365100091f72f8b870c2b9a39a862
Zak Nelson <zak.nelson@gmail.com>
2013-06-21 00:06:01 -0700

    Use port 80 for socket server.

--- client/js/services/config-service.js
+++ client/js/services/config-service.js
@@ -5,5 +5,5 @@ angular.module("hikeio").
 		this.isProd = $location.host() === "hike.io";
 		this.hikeImagesPath = this.isProd ? "http://assets.hike.io/hike-images" : "/hike-images";
 		this.landingPageImagesPath = this.isProd ? "http://assets.hike.io/landing-page-images" : "/landing-page-images";
-		this.socketIoPath = this.isProd ? "http://socket.hike.io:8080" : "http://localhost:8080";
+		this.socketIoPath = this.isProd ? "http://socket.hike.io" : "http://localhost:8080";
 	}]);
