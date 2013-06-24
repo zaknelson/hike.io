c00eddad64574a86aeda024902b987e984189153
Zak Nelson <zak.nelson@gmail.com>
2013-06-23 19:22:11 -0700

    Use string instead of symbols to access environmental variables.

--- server/routes/api_routes.rb
+++ server/routes/api_routes.rb
@@ -231,8 +231,8 @@ class HikeApp < Sinatra::Base
 
 	def s3
 		@s3 = @s3 || AWS::S3.new(
-			:access_key_id     => ENV[:S3_ACCESS_KEY_ID],
-			:secret_access_key => ENV[:S3_SECRET_ACCESS_KEY]
+			:access_key_id     => ENV["S3_ACCESS_KEY_ID"],
+			:secret_access_key => ENV["S3_SECRET_ACCESS_KEY"]
 		)
 		@s3
 	end
