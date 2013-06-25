2514214bfba5a75c243678b3e28198fff600d965
Zak Nelson <zak.nelson@gmail.com>
2013-06-25 01:07:55 -0700

    Allows for www.* subdomains in isProd conditional

--- client/js/services/config-service.js
+++ client/js/services/config-service.js
@@ -2,7 +2,7 @@
 
 angular.module("hikeio").
 	service("config", ["$location", function($location) {
-		this.isProd = $location.host() === "hike.io";
+		this.isProd = $location.host() === "hike.io" || $location.host() === "www.hike.io";
 		this.hikeImagesPath = this.isProd ? "http://assets.hike.io/hike-images" : "/hike-images";
 		this.landingPageImagesPath = this.isProd ? "http://assets.hike.io/landing-page-images" : "/landing-page-images";
 		this.socketIoPath = this.isProd ? "http://socket.hike.io" : "http://localhost:8080";
