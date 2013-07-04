1aa90f8b81856686ceaec115da5448d56680ba5f
Zak Nelson <zak.nelson@gmail.com>
2013-07-04 00:52:10 -0700

    Cleanup hike already exists test to use helper functions.

--- server/test/routes/api_routes_test.rb
+++ server/test/routes/api_routes_test.rb
@@ -110,9 +110,7 @@ class ApiRoutesTest < HikeAppTestCase
 	def test_post_with_hike_that_already_exists
 		data = get_post_hike_json
 		data["name"] = "Empty"
-		set_admin_cookie
-		post "/api/v1/hikes", data.to_json
-		assert_equal 409, last_response.status
+		post_and_validate data, 409
 	end
 
 
