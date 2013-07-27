8930e9cf44d21b3d9fb3fa778fcfb95674838867
Zak Nelson <zak.nelson@gmail.com>
2013-07-27 00:22:26 -0700

    Use photo's width / height to create placeholders for images while they're downloading.

--- client/js/directives/photo-stream-directive.js
+++ client/js/directives/photo-stream-directive.js
@@ -2,14 +2,13 @@
 
 angular.module("hikeio").
 	directive("photoStream", ["$document", "$rootScope", "$timeout", "config", function($document, $rootScope, $timeout, config) {
-
 		var normalImage = "small";
 		var biggerImage = "medium";
 		var template = "<div class='preview-list'>" +
 			"<a href='/hikes/{{hike.string_id}}' data-ng-repeat='hike in hikes'>" +
-				"<div class='preview' >" +
+				"<div class='preview'>" +
 					"<div data-ng-class='{\"featured-box\": $first}' >" +
-						"<img data-ng-src='" + config.hikeImagesPath + "/{{hike.photo_preview.string_id}}{{ $first && \"-" + biggerImage + "\" || \"-" + normalImage + "\" }}.jpg' alt='{{hike.photo_preview.alt}}'></img>" +
+						"<img data-ng-src='" + config.hikeImagesPath + "/{{hike.photo_preview.string_id}}{{ $first && \"-" + biggerImage + "\" || \"-" + normalImage + "\" }}.jpg' data-aspect-ratio='{{hike.photo_preview.height / hike.photo_preview.width}}' alt='{{hike.photo_preview.alt}}'></img>" +
 						"<div class='preview-footer'>" +
 							"<div>" +
 								"<h4 class='preview-title'>{{hike.name}}</h4>" +
@@ -32,28 +31,32 @@ angular.module("hikeio").
 			link: function (scope, element) {
 				var gutterWidth = 2;
 				var maxColumnWidth = 400;
-
 				scope.$watch("hikes", function(newValue, oldValue) {
 					if (newValue.length === 0) return;
-					$document.ready(function() {
-						$timeout(function() {
-							element.imagesLoaded(function(images, proper, broken) {
-								element.css("opacity", "1");
-								element.masonry({
-									itemSelector: ".preview",
-									gutterWidth: gutterWidth,
-									isAnimated: true,
-									columnWidth: function(containerWidth) {
-										var boxes = Math.ceil(containerWidth / maxColumnWidth);
-										var boxWidth = Math.floor((containerWidth - (boxes - 1) * gutterWidth) / boxes);
-										element.find(".preview > div").width(boxWidth);
-										if (boxes !== 1) {
-											element.find(".preview > .featured-box").width(boxWidth * 2 + gutterWidth);
-										}
-										return boxWidth;
-									}
+					element.css("opacity", "1");
+					$timeout(function() {
+						element.masonry({
+							itemSelector: ".preview",
+							gutterWidth: gutterWidth,
+							isAnimated: true,
+							columnWidth: function(containerWidth) {
+								var boxes = Math.ceil(containerWidth / maxColumnWidth);
+								var boxWidth = Math.floor((containerWidth - (boxes - 1) * gutterWidth) / boxes);
+
+								// TODO: clean up these selectors
+								element.find(".preview > div").width(boxWidth);
+								element.find(".preview > div > img").each(function(i, img) {
+									var aspectRatio = parseFloat($(img).attr("data-aspect-ratio"), 10);
+									$(img).height(aspectRatio * boxWidth);
 								});
-							});
+								if (boxes !== 1) {
+									element.find(".preview > div.featured-box").width(boxWidth * 2 + gutterWidth);
+									var featuredBoxImage = element.find(".preview > .featured-box > img");
+									var aspectRatio = parseFloat(featuredBoxImage.attr("data-aspect-ratio"), 10);
+									featuredBoxImage.height(aspectRatio * boxWidth * 2);
+								}
+								return boxWidth;
+							}
 						});
 					});
 				}, true);
