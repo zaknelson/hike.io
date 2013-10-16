fc2178a45b1751d85bbf4d0e93ef21f2b0bb3a38
Zak Nelson <zak.nelson@gmail.com>
2013-10-15 23:35:51 -0700

    Cache added version for users who need reviews.

--- client/js/controllers/add-controller.js
+++ client/js/controllers/add-controller.js
@@ -1,11 +1,15 @@
 "use strict";
-var AddController = function($http, $log, $scope, navigation) {
+var AddController = function($http, $log, $scope, navigation, resourceCache) {
 	$scope.hike = {};
 	$scope.hike.location = {};
 
 	$scope.add = function() {
 		$http({method: "POST", url: "/api/v1/hikes", data: $scope.hike}).
 			success(function(data, status, headers, config) {
+				if (status === 202) {
+					// Keep this temporary version in the user's session cache, in case they decide to make other changes.
+					resourceCache.put("/api/v1/hikes/" + $scope.hike.string_id, jQuery.extend(true, {}, $scope.hike));
+				}
 				navigation.toEntry(data.string_id);
 			}).
 			error(function(data, status, headers, config) {
@@ -16,4 +20,4 @@ var AddController = function($http, $log, $scope, navigation) {
 	$scope.htmlReady();
 };
 
-AddController.$inject = ["$http", "$log", "$scope", "navigation"];
+AddController.$inject = ["$http", "$log", "$scope", "navigation", "resourceCache"];
