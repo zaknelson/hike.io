0656b4d83f1779ec99fe7d431f2b87f06f596785
Zak Nelson <zak.nelson@gmail.com>
2013-10-23 22:56:55 -0700

    Simplify /hikes to just include a list of hikes.

--- client/js/controllers/all-controller.js
+++ client/js/controllers/all-controller.js
@@ -1,27 +1,15 @@
 "use strict";
 var AllController = function($scope, $http, $log, analytics, resourceCache) {
+	$scope.hikes = [];
+	$scope.htmlReady();
+
 	$http({method: "GET", url: "/api/v1/hikes?fields=locality,name,string_id", cache: resourceCache}).
 		success(function(data, status, headers, config) {
-			var localityMap = {};
-			var localities = [];
-			for (var i  = 0; i < data.length; i++) {
-				var hike = data[i];
-				var locality = localityMap[hike.locality];
-				if (!locality) {
-					locality = { name: hike.locality, hikes: [] };
-					localities.push(locality);
-					localityMap[locality.name] = locality;
-				}
-				locality.hikes.push(hike);
-			}
-			$scope.localities = localities;
+			$scope.hikes = data;
 		}).
 		error(function(data, status, headers, config) {
 			$log.error(config);
 		});
-
-	$scope.localities = [];
-	$scope.htmlReady();
 };
 
 AllController.$inject = ["$scope", "$http", "$log", "analytics", "resourceCache"];
