8858f5b42565e56ba2c896666f46f96d3e404f08
Zak Nelson <zak.nelson@gmail.com>
2013-11-09 08:34:39 -0800

    Fix jshint errors.

--- client/js/services/capabilities-service.js
+++ client/js/services/capabilities-service.js
@@ -2,12 +2,13 @@
 
 angular.module("hikeio").
 	service("capabilities", ["$window", function($window) {
+		/* global UAParser: true */
 		var userAgent = new UAParser().getResult();
 
 		//http://stackoverflow.com/questions/7843150/detect-if-browser-supports-contenteditable
 		this.isEditPageSupported = true;
-		if ((userAgent.os.name === "Android" && parseFloat(userAgent.os.version) <= 2.3) || 
-			(userAgent.os.name === "iOS" && parseFloat(userAgent.os.version) < 5) || 
+		if ((userAgent.os.name === "Android" && parseFloat(userAgent.os.version) <= 2.3) ||
+			(userAgent.os.name === "iOS" && parseFloat(userAgent.os.version) < 5) ||
 			(!("contentEditable" in $window.document.documentElement))) {
 			this.isEditPageSupported = false;
 		}
