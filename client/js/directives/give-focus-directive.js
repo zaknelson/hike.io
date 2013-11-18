bfe5961922d3b73e1f3dfef126bab4c0064a8afd
Zak Nelson <zak.nelson@gmail.com>
2013-11-17 18:05:34 -0800

    Set focus to the end of the input when giving focus, matters on the preopulated add dialog.

--- client/js/directives/give-focus-directive.js
+++ client/js/directives/give-focus-directive.js
@@ -8,6 +8,14 @@ angular.module("hikeio").
 				if (value) {
 					$timeout(function() {
 						element.focus();
+
+						// Set focus to the end of the input
+						// http://stackoverflow.com/questions/1056359/set-mouse-focus-and-move-cursor-to-end-of-input-using-jquery
+						if (element.is("input")) {
+							var val = element.val();
+							element.val("");
+							element.val(val);
+						}
 					});
 				}
 			});
