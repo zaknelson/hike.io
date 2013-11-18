243a80eae97160feab61d80cd709fe7adec10216
Zak Nelson <zak.nelson@gmail.com>
2013-11-18 13:50:42 -0800

    Do not use atob to obfuscate mailto, it doesn't work on IE9.

--- client/js/directives/mailto-directive.js
+++ client/js/directives/mailto-directive.js
@@ -4,8 +4,7 @@ angular.module("hikeio").
 	directive("mailto", function() {
 		return {
 			link: function (scope, element, attrs) {
-				// Obfuscate mailto:contact@hike.io
-				element.attr("href", atob("bWFpbHRvOmNvbnRhY3RAaGlrZS5pbw=="));
+				element.attr("href", "mailto:contact@hike.io");
 			}
 		};
 	});
