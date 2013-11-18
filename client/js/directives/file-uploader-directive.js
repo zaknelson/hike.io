4de31e79ad73613745610aa52e632522247c9b98
Zak Nelson <zak.nelson@gmail.com>
2013-11-17 23:30:52 -0800

    Only accept jpgs and pngs as file inputs.

--- client/js/directives/file-uploader-directive.js
+++ client/js/directives/file-uploader-directive.js
@@ -5,7 +5,7 @@ angular.module("hikeio").
 	return {
 		compile: function(tplElm, tplAttr) {
 			var mulitpleStr = tplAttr.multiple === "true" ? "multiple" : "";
-			tplElm.after("<input type='file' " + mulitpleStr + " style='display: none;'>");
+			tplElm.after("<input type='file' " + mulitpleStr + " accept='image/png, image/jpeg' style='display: none;'>");
 
 			return function(scope, elm, attr) {
 				if (scope.$eval(attr.enabled)) {
