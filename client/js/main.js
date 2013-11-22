3c019df8f3932377dd0ca411a8b58facb43fc90d
Zak Nelson <zak.nelson@gmail.com>
2013-11-21 22:56:24 -0800

    Update documentation for disabling angluarjs.

--- client/js/main.js
+++ client/js/main.js
@@ -45,7 +45,7 @@ angular.module("hikeio", ["seo", "ui"]).
 			});
 	}]).
 	run(["$http", "$location", "$rootScope", "$templateCache", "$timeout", "$window", "capabilities", "config", "navigation", function($http, $location, $rootScope, $templateCache, $timeout, $window, capabilities, config, navigation) {
-		// HACK, if url parameters include _escaped_fragment_ we are being indexed and already have all of the html rendered
+		// HACK, if url parameters include _escaped_fragment_ this request is being made by a crawler and the html is already rendered.
 		// If angular starts to render again, things won't look right, so throw an exception to essentially disable angular
 		if ($location.search()["_escaped_fragment_"] !== undefined) {
 			throw Error();
