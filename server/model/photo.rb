01701707dcb7e54a858a7b49d9fb5374cab7dbdf
Zak Nelson <zak.nelson@gmail.com>
2013-11-26 10:58:25 -0800

    Wrap write call in Pathname.

--- server/model/photo.rb
+++ server/model/photo.rb
@@ -69,7 +69,7 @@ class Photo < Sequel::Model
 						Photo.each_rendition_including_original do |rendition_name|
 							object_path = dst_dir + name + get_rendition_suffix(rendition_name)
 							rendition_path = file.path + Photo.get_rendition_suffix(rendition_name)
-							bucket.objects[object_path].write(rendition_path)
+							bucket.objects[object_path].write(Pathname.new(rendition_path))
 						end
 					else
 						dst_dir = HikeApp.root + "/public/hike-images/tmp/uploading/"
