7de31513c3eb50a3c0df5e800470675cfa1a53bc
Zak Nelson <zak.nelson@gmail.com>
2013-12-03 09:59:06 -0800

    Send emails on admin changes.

--- server/routes/api_routes.rb
+++ server/routes/api_routes.rb
@@ -19,17 +19,16 @@ class HikeApp < Sinatra::Base
 		string_id = Hike.create_string_id_from_name(json["name"])
 		return 404 if string_id.length == 0
 		return 409 if Hike[:string_id => string_id]
+		if Sinatra::Application.environment() != :test
+			Thread.new { EmailUtils.send_new_review(json_str, string_id, request.base_url, user_needs_changes_reviewed?) }
+		end
 		if user_needs_changes_reviewed?
 			review = Review.create({
 				:api_verb => "post",
 				:api_body => json_str,
 				:hike_string_id => string_id,
 				:reviewee => current_user_id
-			})
-			if Sinatra::Application.environment() != :test
-				Thread.new { EmailUtils.send_new_review(review, request.base_url) }
-			end
-			
+			})			
 			response.headers["Hikeio-Hike-String-Id"] = string_id
 			return 202
 		end
@@ -69,6 +68,9 @@ class HikeApp < Sinatra::Base
 		return 400 if not Hike.is_valid_json? json
 		return 409 if json["string_id"] && json["string_id"] != params[:hike_id] && Hike.get_hike_from_id(json["string_id"])
 		return 404 if !hike && !Review.has_pending_review_for_hike?(params[:hike_id])
+		if Sinatra::Application.environment() != :test
+			Thread.new { EmailUtils.send_diff_review(json_str, params[:hike_id], request.base_url, user_needs_changes_reviewed?) }
+		end
 		if user_needs_changes_reviewed? 
 			review = Review.create({
 				:api_verb => "put",
@@ -76,9 +78,6 @@ class HikeApp < Sinatra::Base
 				:hike_string_id => params[:hike_id],
 				:reviewee => current_user_id
 			})
-			if Sinatra::Application.environment() != :test
-				Thread.new { EmailUtils.send_diff_review(review, request.base_url) }
-			end
 			return 202
 		end
 		return 409 if hike.edit_time.to_s != json["edit_time"]
@@ -88,15 +87,15 @@ class HikeApp < Sinatra::Base
 
 	delete "/api/v1/hikes/:hike_id", :provides => "json" do
 		hike = Hike.get_hike_from_id params[:hike_id]
+		if Sinatra::Application.environment() != :test
+			Thread.new { EmailUtils.send_delete_review(params[:hike_id], request.base_url, user_needs_changes_reviewed?) }
+		end
 		if user_needs_changes_reviewed?
 			review = Review.create({
 				:api_verb => "delete",
 				:hike_string_id => params[:hike_id],
 				:reviewee => current_user_id
 			})
-			if Sinatra::Application.environment() != :test
-				Thread.new { EmailUtils.send_delete_review(review, request.base_url) }
-			end
 			return 202
 		elsif not hike
 			return 404
--- server/utils/email_utils.rb
+++ server/utils/email_utils.rb
@@ -6,17 +6,19 @@ require_relative "../model/hike"
 
 class EmailUtils
 
-	def self.send_review review, body, base_url, title
+	def self.send_review string_id, body, base_url, title, needs_review
 		api_key = ENV["MAILGUN_API_KEY"]
 		api_url = "https://api:#{api_key}@api.mailgun.net/v2/hike.io.mailgun.org"
 		html = "<html>"
 		html += "<head><style>#{Diffy::CSS}</style></head>"
 		html += "<body>"
 		html += body
-		html += "<div>"
-		html += '<a href="' + "#{base_url}/admin/v1/reviews/#{review.string_id}/accept" + '" style="color: #fff; text-decoration: none; display: inline-block; position: relative; padding-top: 0; padding-right: 20px; padding-bottom: 0; padding-left: 20px; border: 0; font-size: 18px; line-height: 39px; text-align: center; font-style: normal; cursor: pointer; border-bottom: 1px solid rgba(0 , 0 , 0 , 0.1); font-family: sans-serif; border-radius: 3px; background-color: #009900; margin-top: 5px; margin-right: 5px;">Accept</a>'
-		html += '<a href="' + "#{base_url}/admin/v1/reviews/#{review.string_id}/reject" + '" style="color: #fff; text-decoration: none; display: inline-block; position: relative; padding-top: 0; padding-right: 20px; padding-bottom: 0; padding-left: 20px; border: 0; font-size: 18px; line-height: 39px; text-align: center; font-style: normal; cursor: pointer; border-bottom: 1px solid rgba(0 , 0 , 0 , 0.1); font-family: sans-serif; border-radius: 3px; background-color: #CC0000; margin-top: 5px; margin-right: 5px;">Reject</a>'
-		html += "</div>"
+		if needs_review
+			html += "<div>"
+			html += '<a href="' + "#{base_url}/admin/v1/reviews/#{string_id}/accept" + '" style="color: #fff; text-decoration: none; display: inline-block; position: relative; padding-top: 0; padding-right: 20px; padding-bottom: 0; padding-left: 20px; border: 0; font-size: 18px; line-height: 39px; text-align: center; font-style: normal; cursor: pointer; border-bottom: 1px solid rgba(0 , 0 , 0 , 0.1); font-family: sans-serif; border-radius: 3px; background-color: #009900; margin-top: 5px; margin-right: 5px;">Accept</a>'
+			html += '<a href="' + "#{base_url}/admin/v1/reviews/#{string_id}/reject" + '" style="color: #fff; text-decoration: none; display: inline-block; position: relative; padding-top: 0; padding-right: 20px; padding-bottom: 0; padding-left: 20px; border: 0; font-size: 18px; line-height: 39px; text-align: center; font-style: normal; cursor: pointer; border-bottom: 1px solid rgba(0 , 0 , 0 , 0.1); font-family: sans-serif; border-radius: 3px; background-color: #CC0000; margin-top: 5px; margin-right: 5px;">Reject</a>'
+			html += "</div>"
+		end
 		html += "</body>"
 		html += "</html>"
 		RestClient.post api_url + "/messages", 
@@ -26,26 +28,26 @@ class EmailUtils
 			:html => Premailer.new(html, :with_html_string => true).to_inline_css
 	end
 
-	def self.send_new_review(review, base_url)
-		new_json = JSON.pretty_generate(JSON.parse(review.api_body))
-		send_review(review, new_json, base_url, "New hike")
+	def self.send_new_review(json_str, string_id, base_url, needs_review)
+		new_json = JSON.pretty_generate(JSON.parse(json_str))
+		send_review(string_id, new_json, base_url, "New hike", needs_review)
 	end
 
-	def self.send_diff_review(review, base_url)
-		hike = Hike[:string_id => review.hike_string_id]
-		title = "Update for #{review.hike_string_id}"
+	def self.send_diff_review(json_str, string_id, base_url, needs_review)
+		hike = Hike[:string_id => string_id]
+		title = "Update for #{string_id}"
 		# Because the review process allows users to perform updates on hikes that haven't been
 		# created yet, there might not yet be a before.
 		before = hike ? JSON.pretty_generate(JSON.parse(hike.as_json)) : ""
-		after = JSON.pretty_generate(JSON.parse(review.api_body))
+		after = JSON.pretty_generate(JSON.parse(json_str))
 		html = Diffy::Diff.new(before, after).to_s(:html)
-		send_review(review, html, base_url, title)
+		send_review(string_id, html, base_url, title, needs_review)
 	end
 
-	def self.send_delete_review(review, base_url)
-		hike = Hike[:string_id => review.hike_string_id]
-		title = "Delete for #{review.hike_string_id}"
+	def self.send_delete_review(string_id, base_url, needs_review)
+		hike = Hike[:string_id => string_id]
+		title = "Delete for #{string_id}"
 		body = hike ? "<a href='#{base_url}/hikes/#{hike.string_id}'>#{base_url}/hikes/#{hike.string_id}</a>" : "<p>Hike doesn't yet exist</p>"
-		send_review(review, body, base_url, title)
+		send_review(string_id, body, base_url, title, needs_review)
 	end
 end
