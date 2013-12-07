6e06a5bff80f96fe797aff327d1500d77c1be0c7
Zak Nelson <zak.nelson@gmail.com>
2013-12-06 18:14:09 -0800

    Update traverse_assets script with photo cleanup code.

--- scripts/s3/traverse_assets.rb
+++ scripts/s3/traverse_assets.rb
@@ -77,6 +77,73 @@ def set_cache_control object
 	end
 end
 
+def delete_abdandoned_photos_on_s3 bucket
+	bucket = s3.buckets["assets.hike.io"]
+	s3_photos = []
+	i = 0
+	bucket.objects.each do |object|
+		#next if i > 5
+		key = object.key
+		if key.start_with?("hike-images/") and key.end_with?("-original.jpg")
+			i += 1
+			index_of_first_slash = key.index("/")
+			id = key[index_of_first_slash + 1, key.length - index_of_first_slash]
+			id.chomp! "-original.jpg"
+			s3_photos.push(id)
+		end
+	end
+
+	db_photos = [] # these are just the db photos that are assigned to hikes
+	database["SELECT photos.string_id FROM hikes, photos where hikes.photo_facts_id=photos.id OR hikes.photo_landscape_id=photos.id OR hikes.photo_preview_id=photos.id"].entries.each do |photo|
+		db_photos.push(photo[:string_id])
+	end
+	database["SELECT photos.string_id FROM hikes_photos, photos WHERE hikes_photos.photo_id=photos.id"].entries.each do |photo|
+		db_photos.push(photo[:string_id])
+	end
+
+	diff = s3_photos - db_photos
+	puts s3_photos.length
+	puts db_photos.length
+	puts diff.length
+	puts diff
+
+	#diff.each do |photo_id|
+	#	bucket.objects["hike-images/" + photo_id + "-original.jpg"].delete
+	#	bucket.objects["hike-images/" + photo_id + "-large.jpg"].delete
+	#	bucket.objects["hike-images/" + photo_id + "-medium.jpg"].delete
+	#	bucket.objects["hike-images/" + photo_id + "-small.jpg"].delete
+	#	bucket.objects["hike-images/" + photo_id + "-tiny.jpg"].delete
+	#	bucket.objects["hike-images/" + photo_id + "-thumb.jpg"].delete
+	#	bucket.objects["hike-images/" + photo_id + "-thumb-tiny.jpg"].delete
+	#end
+end
+
+def delete_photos_in_db_that_are_unassigned
+
+	assigned_db_photos = [] # these are just the db photos that are assigned to hikes
+	database["SELECT photos.string_id FROM hikes, photos where hikes.photo_facts_id=photos.id OR hikes.photo_landscape_id=photos.id OR hikes.photo_preview_id=photos.id ORDER BY photos.string_id"].entries.each do |photo|
+		assigned_db_photos.push(photo[:string_id])
+	end
+	database["SELECT photos.string_id FROM hikes_photos, photos WHERE hikes_photos.photo_id=photos.id ORDER BY photos.string_id"].entries.each do |photo|
+		assigned_db_photos.push(photo[:string_id])
+	end
+
+	all_db_photos = []
+	database["SELECT string_id FROM photos ORDER BY string_id"].entries.each do |photo|
+		all_db_photos.push(photo[:string_id])
+	end
+
+	diff = all_db_photos - assigned_db_photos
+
+	puts all_db_photos.length
+	puts assigned_db_photos.length
+	puts diff.length
+	puts diff
+	diff.each do |photo_string_id|
+		#puts database["DELETE FROM photos WHERE string_id='#{photo_string_id}'"].entries
+	end
+end
+
 def trace object
 	key = object.key
 	puts key
@@ -84,6 +151,8 @@ end
 
 def main
 	bucket = s3.buckets["assets.hike.io"]
+	#delete_abdandoned_photos_on_s3 bucket
+	#delete_photos_in_db_that_are_unassigned
 	bucket.objects.each do |object|
 		trace object
 		#strip_metadata object
