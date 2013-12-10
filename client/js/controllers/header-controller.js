2332aac0e440e2d72f4bb6bf80f7fe70b888ee5a
Zak Nelson <zak.nelson@gmail.com>
2013-12-09 16:19:24 -0800

    Dismiss keyboard on ios by blurring active element when user is searching.

--- client/js/controllers/header-controller.js
+++ client/js/controllers/header-controller.js
@@ -1,11 +1,12 @@
 "use strict";
-var HeaderController = function($scope, search) {
+var HeaderController = function($scope, $window, search) {
 	$scope.searchQuery = "";
 
 	$scope.search = function(event) {
+		$window.document.activeElement.blur();
 		search.search($scope.searchQuery);
 		$scope.searchQuery = "";
 	};
 };
 
-HeaderController.$inject = ["$scope", "search"];
+HeaderController.$inject = ["$scope", "$window", "search"];
