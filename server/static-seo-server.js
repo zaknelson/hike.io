73039f3e49dc732289f410025e5132d7b599198f
Zak Nelson <zak.nelson@gmail.com>
2013-12-11 00:46:39 -0800

    Cleanup static html.

--- server/static-seo-server.js
+++ server/static-seo-server.js
@@ -7,11 +7,39 @@ if (system.args.length < 2) {
 }
 var url = system.args[1];
 
+// Search engines are getting tripped up on some of the angular magic. For example, it thinks one of the primary keywords
+// of the site is string_id. In order to combat this, cleanup some non visible elements from the static html.
+var cleanup = function(page) {
+	var result = page.evaluate(function () {
+		var allElements = document.getElementsByTagName("*");
+		var dataPrefix = "data-";
+		for (var i = allElements.length - 1; i >= 0; i--) {
+			var element = allElements[i];
+			var attributes = element.attributes;
+			var attributesLength = attributes.length;
+			for (var j = attributesLength - 1; j >=0; j--) {
+				var attribute = attributes[j].name;
+				var attributeValue = attributes[j].value;
+				if ((attribute === "type" && attributeValue === "text/ng-template") || 
+					(attribute === "data-static-html-hidden" && attributeValue === "true") ||
+					attribute === "data-preload-resource") {
+					element.parentNode.removeChild(element);
+					continue;
+				}
+				if (attribute.slice(0, dataPrefix.length) === dataPrefix) { // remove if it has the data prefix
+					element.removeAttribute(attribute);
+				}
+			}
+		}
+	});
+};
+
 var renderHtml = function(url) {
 	var page = require("webpage").create();
 	page.settings.loadImages = false;
 	page.settings.localToRemoteUrlAccessEnabled = true;
 	page.onCallback = function() {
+		cleanup(page);
 		setTimeout(function() {
 			console.log(page.content);
 			phantom.exit();
