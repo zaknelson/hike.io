3f69d0856327864c889402f836824c9f4ab74197
Zak Nelson <zak.nelson@gmail.com>
2013-12-13 23:14:58 -0800

    Include photo_facts in /discover request so that it is cached.

--- client/js/controllers/photo-stream-controller.js
+++ client/js/controllers/photo-stream-controller.js
@@ -1,7 +1,7 @@
 "use strict";
 var PhotoStreamController = function($scope, $http, $log, $timeout, analytics, resourceCache) {
 
-	$http({method: "GET", url: "/api/v1/hikes?fields=distance,locality,name,photo_preview,photo_facts,string_id", cache: resourceCache}).
+	$http({method: "GET", url: "/api/v1/hikes?fields=distance,locality,name,photo_facts,photo_preview,string_id", cache: resourceCache}).
 		success(function(data, status, headers, config) {
 			// Only show the hikes that have photo previews or facts photo, which is a backup option
 			var hikes = jQuery.grep(data, function(hike){
