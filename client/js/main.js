bd8c175af098263a6768501851849fcb76e67291
Zak Nelson <zak.nelson@gmail.com>
2013-12-14 01:43:25 -0800

    Request all hikes in background on first load, that way /discover page shows up even quicker.

--- client/js/main.js
+++ client/js/main.js
@@ -44,7 +44,7 @@ angular.module("hikeio", ["seo", "ui"]).
 				}
 			});
 	}]).
-	run(["$http", "$location", "$rootScope", "$templateCache", "$timeout", "$window", "capabilities", "config", "navigation", function($http, $location, $rootScope, $templateCache, $timeout, $window, capabilities, config, navigation) {
+	run(["$http", "$location", "$rootScope", "$templateCache", "$timeout", "$window", "capabilities", "config", "navigation", "resourceCache", function($http, $location, $rootScope, $templateCache, $timeout, $window, capabilities, config, navigation, resourceCache) {
 		// HACK, if url parameters include _escaped_fragment_ this request is being made by a crawler and the html is already rendered.
 		// If angular starts to render again, things won't look right, so throw an exception to essentially disable angular
 		if ($location.search()._escaped_fragment_ !== undefined) {
@@ -70,13 +70,14 @@ angular.module("hikeio", ["seo", "ui"]).
 			}
 		});
 
-		// Pre-populate template
+		// Pre-populate caches
 		$timeout(function() {
-			$http.get("/partials/entry.html",			{ cache:$templateCache });
-			$http.get("/partials/index.html",			{ cache:$templateCache });
-			$http.get("/partials/photo_stream.html",	{ cache:$templateCache });
-			$http.get("/partials/map.html",				{ cache:$templateCache });
-			$http.get("/partials/search.html",			{ cache:$templateCache });
+			$http.get("/partials/entry.html",			{ cache: $templateCache });
+			$http.get("/partials/index.html",			{ cache: $templateCache });
+			$http.get("/partials/photo_stream.html",	{ cache: $templateCache });
+			$http.get("/partials/map.html",				{ cache: $templateCache });
+			$http.get("/partials/search.html",			{ cache: $templateCache });
+			$http.get("/api/v1/hikes?fields=distance,locality,name,photo_facts,photo_preview,string_id", { cache: resourceCache} )
 		}, 1000);
 
 		// IE 9 does its own caching, and requests are not hitting the server
