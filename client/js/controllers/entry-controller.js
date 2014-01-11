86739f9fd562ff80e5dd141413aef42b5c5210e9
Zak Nelson <zak.nelson@gmail.com>
2014-01-11 01:28:49 -0800

    Alert edit_time in order to further debug issue.

--- client/js/controllers/entry-controller.js
+++ client/js/controllers/entry-controller.js
@@ -137,7 +137,7 @@ var EntryController = function($filter, $http, $log, $rootScope, $routeParams, $
 					if (dateTime.after(data.edit_time, cachedHike.edit_time)) {
 						persistentStorage.remove(routeId);
 					} else {
-						$window.alert("cached");
+						$window.alert(cachedHike.edit_time);
 						hike = cachedHike;
 					}
 				}
