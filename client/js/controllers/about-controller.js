1cf401cdaa268f2f0572d0ddf024fc7a87e7e52d
Zak Nelson <zak.nelson@gmail.com>
2014-01-14 01:28:57 -0800

    Add a cute animation on /about page.

--- client/js/controllers/about-controller.js
+++ client/js/controllers/about-controller.js
@@ -1,6 +1,14 @@
 "use strict";
-var AboutController = function($scope, analytics) {
+var AboutController = function($scope, $window, analytics) {
+	$scope.scrolledToSubPageTwo = false;
+	$window.onscroll = function() {
+		if (!$scope.scrolledToSubPageTwo && $window.pageYOffset > $($window).height() * 0.75) {
+			$scope.$apply(function() {
+				$scope.scrolledToSubPageTwo = true;
+			})
+		}
+	};
 	$scope.htmlReady();
 };
 
-AboutController.$inject = ["$scope", "analytics"];
+AboutController.$inject = ["$scope", "$window", "analytics"];
