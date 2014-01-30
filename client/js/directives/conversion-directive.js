d54775738422f1150056b9440a7b22f3668ff8da
Zak Nelson <zak.nelson@gmail.com>
2014-01-30 00:54:06 -0800

    Refactor unit conversion code.

--- client/js/directives/conversion-directive.js
+++ client/js/directives/conversion-directive.js
@@ -9,10 +9,11 @@ angular.module("hikeio").
 				var truncateTo = attrs.truncateTo;
 				var hideDecimalAt = attrs.hideDecimalAt;
 				var showTrailingZeroes = attrs.showTrailingZeroes;
+				if (!units) return;
 				$rootScope.$watch("preferences.useMetric", function(useMetric) {
 					var convertedUnits = units;
-					if (useMetric !== conversion.isMetric(units)) {
-						convertedUnits = conversion.getCorrespondingUnits(units);
+					if (useMetric !== conversion.isMetricUnits(units)) {
+						convertedUnits = conversion.getOpposingUnits(units);
 					}
 					var convertedValue = conversion.convert(value, units, convertedUnits, truncateTo, hideDecimalAt, showTrailingZeroes);
 					if (convertedValue === "1") {
--- client/js/services/conversion-service.js
+++ client/js/services/conversion-service.js
@@ -3,97 +3,68 @@
 angular.module("hikeio").
 	factory("conversion", function() {
 
-		var CONVERT_UNITS = {
-			"meters": "feet",
-			"feet": "meters",
-			"kilometers": "miles",
-			"miles": "kilometers",
-
-			"meter": "feet",
-			"foot": "meters",
-			"kilometer": "miles",
-			"mile": "kilometers",
-
-			"m": "ft",
-			"ft": "m",
-			"km": "mi",
-			"mi": "km"
-		};
-
-		var CANONICAL_UNITS = {
-			"m": "meters",
-			"ft": "feet",
-			"km": "kilometers",
-			"mi": "miles",
-
-			"meter": "meters",
-			"foot": "feet",
-			"kilometer": "kilometers",
-			"mile": "miles"
-		};
-
-		var METRIC_UNITS = {
-			"km": true,
-			"kilometers": true,
-			"kilometer": true,
-			"m": true,
-			"meters": true,
-			"meter": true
-		};
-
-		var SINGULAR_UNITS = {
-			"feet": "foot",
-			"meters": "meter",
-			"miles" : "mile",
-			"kilometers": "kilometer"
-		};
-
 		var ConversionService = function() {
-		};
+			this.unitFormats = {};
+			var formats = ["abbreviated", "singular", "plural"];
+			this.units = {
+				"Kilometer":	{ type: "Kilometer",	abbreviated: "km",		singular : "kilometer",	plural: "kilometers",	isMetric: true,		opposition: "Mile",			ratio: 0.62137119223 },
+				"Meter":		{ type: "Meter",		abbreviated: "m",		singular : "meter",		plural: "meters",		isMetric: true,		opposition: "Feet",			ratio: 3.28083989501 },
+				"Mile":			{ type: "Mile",			abbreviated: "mi",		singular : "mile",		plural: "miles",		isMetric: false,	opposition: "Kilometer",	ratio: 1.609344 },
+				"Feet":			{ type: "Feet",			abbreviated: "ft",		singular : "foot",		plural: "feet",			isMetric: false,	opposition: "Meter",		ratio: 0.3048 }
+			};
 
-		ConversionService.prototype.getCorrespondingUnits = function(units) {
-			return CONVERT_UNITS[units];
-		};
-
-		ConversionService.prototype.isMetric = function(units) {
-			return METRIC_UNITS[units] === true;
-		};
-
-		ConversionService.prototype.getSingularUnits = function(units) {
-			var singular = SINGULAR_UNITS[units];
-			if (singular) {
-				return singular;
-			} else {
-				return units;
+			for (var unit in this.units) {
+				if (this.units.hasOwnProperty(unit)) {
+					for (var j = 0; j < formats.length; j++) {
+						var format = formats[j];
+						this.unitFormats[this.units[unit][format]] = {
+							unit: unit,
+							format: format
+						};
+					}
+				}
 			}
+		};
+
+		ConversionService.prototype.parseUnits = function(unitStr) {
+			if (!unitStr) return;
+			var unitFormats = this.unitFormats[unitStr];
+			if (unitFormats) return this.units[unitFormats.unit];
+		};
+
+		ConversionService.prototype.getOpposingUnits = function(unitStr) {
+			if (!unitStr) return;
+			var unitFormats = this.unitFormats[unitStr];
+			if (!unitFormats) return;
+			var format = unitFormats.format;
+			var unit = unitFormats.unit;
+			var opposition = this.units[unit].opposition;
+			return this.units[opposition][format];
+		};
+
+		ConversionService.prototype.isMetricUnits = function(unitStr) {
+			var units = this.parseUnits(unitStr);
+			if (units) return units.isMetric;
+		};
+
+		ConversionService.prototype.getSingularUnits = function(unitStr) {
+			var units = this.parseUnits(unitStr);
+			if (units) return units.singular;
 		};
 
 		ConversionService.prototype.convert = function(value, from, to, truncateTo, hideDecimalAt, showTrailingZeroes) {
-			// Cleanup input
-			if (typeof value !== "number") {
-				value = parseFloat(value);
-			}
-			if (typeof truncateTo !== "number") {
-				truncateTo = parseFloat(truncateTo);
-			}
-			if (typeof hideDecimalAt !== "number") {
-				hideDecimalAt = parseFloat(hideDecimalAt);
-			}
-			from = CANONICAL_UNITS[from] || from;
-			to = CANONICAL_UNITS[to] || to;
-
+			value = parseFloat(value);
+			from = this.parseUnits(from);
+			to = this.parseUnits(to);
+			truncateTo = parseFloat(truncateTo);
+			hideDecimalAt = parseFloat(hideDecimalAt);
+			if (from !== to && from.opposition !== to.type) return value;
 			var result = value;
-			if (from === "meters" && to === "feet") {
-				result = value * 3.28083989501;
-			} else if (from === "feet" && to === "meters") {
-				result = value * 0.30480; // exact
-			} else if (from === "kilometers" && to === "miles") {
-				result = value * 0.62137119223;
-			} else if (from === "miles" && to === "kilometers") {
-				result = value * 1.609344; // exact
+			if (from !== to) {
+				result = value * from.ratio;
 			}
 			var stringResult = null;
-			if (truncateTo) {
+			if (!isNaN(truncateTo)) {
 				stringResult = result.toFixed(truncateTo);
 			} else {
 				if (result < 10) {
