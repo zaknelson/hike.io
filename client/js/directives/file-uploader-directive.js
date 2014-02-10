90edaa363c30b31e42b73e7c29303ffc6d3099bd
Zak Nelson <zak.nelson@gmail.com>
2014-02-10 12:30:54 -0800

    Add descriptions to file inputs, so it's clear whether you're meant to upload a photo or route.

--- client/js/directives/file-uploader-directive.js
+++ client/js/directives/file-uploader-directive.js
@@ -6,8 +6,11 @@ angular.module("hikeio").
 		compile: function(tplElm, tplAttr) {
 			var mulitpleStr = tplAttr.multiple === "true" ? "multiple" : "";
 			var acceptStr = tplAttr.accept ? " accept='" + tplAttr.accept + "'" : "";
+			if (tplAttr.description) {
+				var fontSize = tplAttr.descriptionFontSize || "14";
+				tplElm.append("<div style='position:absolute; top:50%; text-align:center; line-height:100px; pointer-events:none; width:100%; height:100px; margin-top:-50px; font-style:italic; color: #999; font-size: " + fontSize + "px;'>" + tplAttr.description + "</div>");
+			}
 			tplElm.after("<input type='file' " + mulitpleStr + acceptStr + " style='display: none;'>");
-
 			return function(scope, elm, attr) {
 				if (scope.$eval(attr.enabled)) {
 					var input = angular.element(elm[0].nextSibling);
@@ -28,6 +31,7 @@ angular.module("hikeio").
 					});
 
 					elm.css("cursor", "pointer");
+					elm.css("position", "relative");
 				}
 			};
 		},
