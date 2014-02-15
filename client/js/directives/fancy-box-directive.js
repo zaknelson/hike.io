98440353dcec2a1d714c24ffaab574211e2b71dd
Zak Nelson <zak.nelson@gmail.com>
2014-02-14 16:44:22 -0800

    Use shortened versions of licenses and different separators.

--- client/js/directives/fancy-box-directive.js
+++ client/js/directives/fancy-box-directive.js
@@ -6,12 +6,12 @@ angular.module("hikeio").
 			link: function (scope, element, attrs) {
 				var flickrLicenses = {
 					"0": {"name":"All Rights Reserved", "url":"" },
-					"1": {"name":"Attribution-NonCommercial-ShareAlike License", "url":"http://creativecommons.org/licenses/by-nc-sa/2.0/"},
-					"2": {"name":"Attribution-NonCommercial License", "url":"http://creativecommons.org/licenses/by-nc/2.0/"},
-					"3": {"name":"Attribution-NonCommercial-NoDerivs License", "url":"http://creativecommons.org/licenses/by-nc-nd/2.0/"},
-					"4": {"name":"Attribution License", "url":"http://creativecommons.org/licenses/by/2.0/"},
-					"5": {"name":"Attribution-ShareAlike License", "url":"http://creativecommons.org/licenses/by-sa/2.0/"},
-					"6": {"name":"Attribution-NoDerivs License", "url":"http://creativecommons.org/licenses/by-nd/2.0/"},
+					"1": {"name":"Attribution-NonCommercial-ShareAlike License", "short":"CC BY-NC-SA 2.0", "url":"http://creativecommons.org/licenses/by-nc-sa/2.0/"},
+					"2": {"name":"Attribution-NonCommercial License", "short":"CC BY-NC 2.0", "url":"http://creativecommons.org/licenses/by-nc/2.0/"},
+					"3": {"name":"Attribution-NonCommercial-NoDerivs License", "short":"CC BY-NC-ND 2.0", "url":"http://creativecommons.org/licenses/by-nc-nd/2.0/"},
+					"4": {"name":"Attribution License", "short":"CC BY 2.0", "url":"http://creativecommons.org/licenses/by/2.0/"},
+					"5": {"name":"Attribution-ShareAlike License", "short":"CC BY-SA 2.0", "url":"http://creativecommons.org/licenses/by-sa/2.0/"},
+					"6": {"name":"Attribution-NoDerivs License", "short":"CC BY-ND 2.0", "url":"http://creativecommons.org/licenses/by-nd/2.0/"},
 					"7": {"name":"No known copyright restrictions", "url":"http://www.flickr.com/commons/usage/"},
 					"8": {"name":"United States Government Work", "url":"http://www.usa.gov/copyright.shtml"}
 				};
@@ -34,15 +34,16 @@ angular.module("hikeio").
 									if (data.stat === "ok") {
 										var attributionDiv = $("<div class='attribution-string'></div>");
 										var nameAndTitleAnchor = $("<a href='" + fancyboxElement.attr("data-attribution-link") + "'>\"" + data.photo.title._content + "\" by " + data.photo.owner.realname + "<a/>");
-										var separator = $("<span class='separator'> - </span>");
+										var separator = $("<span class='separator'> • </span>");
 										var br = $("<br>");
-										var licenseAnchor = $("<a href='" + flickrLicenses[data.photo.license].url + "'>" + flickrLicenses[data.photo.license].name + "<a/>");
-										var modified = $("<span> - Resized from original </span>");
+										var licenseName = flickrLicenses[data.photo.license].short || flickrLicenses[data.photo.license].name;
+										var licenseAnchor = $("<a href='" + flickrLicenses[data.photo.license].url + "'> • " + licenseName + "<a/>");
+										var modified = $("<span> Resized from original </span>");
 										attributionDiv.append(nameAndTitleAnchor);
 										attributionDiv.append(br);
 										attributionDiv.append(separator);
-										attributionDiv.append(licenseAnchor);
 										attributionDiv.append(modified);
+										attributionDiv.append(licenseAnchor);
 										$(".fancybox-inner").append(attributionDiv);
 										$timeout(function() {
 											attributionDiv.css("opacity", "1");
