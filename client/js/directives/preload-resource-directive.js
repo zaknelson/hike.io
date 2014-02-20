68445f499c85ea57311b7edad5a8fe00f2639ee9
Zak Nelson <zak.nelson@gmail.com>
2014-02-20 01:36:16 -0800

    Add another tacky workaround for value getting escaped by DOM.

--- client/js/directives/preload-resource-directive.js
+++ client/js/directives/preload-resource-directive.js
@@ -6,7 +6,7 @@ angular.module("hikeio").
 			link: function (scope, element, attrs) {
 				// # HACKY, browser is doing some processing on the html before I can get to it, 
 				// even though it is properly escaped on the backend.
-				var unescaped = element.html().replace(/"\\&quot;|\\&quot;"/g, "\\\"");
+				var unescaped = element.html().replace(/"\\&quot;|\\&quot;"/g, "\\\"").replace(/&amp;/g, "&");
 				resourceCache.put(attrs.preloadResource, unescaped);
 			}
 		};
