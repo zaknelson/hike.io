88799f4498f0d0e6fb367cdd52e1705bd3f56cb2
Zak Nelson <zak.nelson@gmail.com>
2014-02-21 03:03:16 -0800

    Fix closing </a>

--- client/js/directives/fancy-box-directive.js
+++ client/js/directives/fancy-box-directive.js
@@ -36,7 +36,7 @@ angular.module("hikeio").
 									if (data.stat === "ok") {
 										var attributionDiv = $("<div class='attribution-string'></div>");
 										var authorName = data.photo.owner.realname ? data.photo.owner.realname : data.photo.owner.username;
-										var nameAndTitleAnchor = $("<a href='" + fancyboxElement.attr("data-attribution-link") + "'>\"" + data.photo.title._content + "\" by " + authorName + "<a/>");
+										var nameAndTitleAnchor = $("<a href='" + fancyboxElement.attr("data-attribution-link") + "'>\"" + data.photo.title._content + "\" by " + authorName + "</a>");
 										var separator = $("<span class='separator'> • </span>");
 										var br = $("<br>");
 										var licenseName = flickrLicenses[data.photo.license].short || flickrLicenses[data.photo.license].name;
