9ddbb1e0b9e78630dfbeaaa7258dd3b875fb9b26
Zak Nelson <zak.nelson@gmail.com>
2014-10-19 00:52:09 -0700

    Fix preload directive with IE10.

--- client/js/directives/preload-resource-directive.js
+++ client/js/directives/preload-resource-directive.js
@@ -6,7 +6,9 @@ angular.module("hikeio").
 			link: function (scope, element, attrs) {
 				// # HACKY, browser is doing some processing on the html before I can get to it, 
 				// even though it is properly escaped on the backend.
-				var unescaped = element.html().replace(/"\\&quot;|\\&quot;"/g, "\\\"").replace(/&amp;/g, "&");
+				console.log(element.html());
+				var unescaped = element.html().replace(/"\\&quot;|\\&quot;"|'\\"/g, "\\\"").replace(/&amp;/g, "&");
+				console.log(unescaped);
 				resourceCache.put(attrs.preloadResource, unescaped);
 			}
 		};
