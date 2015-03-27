069020d26c2ffc81198a2e9c8e9ac2bfff58d821
Zak Nelson <zak.nelson@gmail.com>
2015-03-26 22:39:30 -0700

    Move attribution code into its own service.

--- client/js/directives/fancy-box-directive.js
+++ client/js/directives/fancy-box-directive.js
@@ -1,68 +1,40 @@
 "use strict";
 
 angular.module("hikeio").
-	directive("fancybox", ["$http", "$log", "$rootScope", "$timeout", "resourceCache", function($http, $log, $rootScope, $timeout, resourceCache) {
+	directive("fancybox", ["$log", "$rootScope", "$timeout", "attribution", function($log, $rootScope, $timeout, attribution) {
 		return {
 			link: function (scope, element, attrs) {
-				var flickrLicenses = {
-					"0": {"name":"All Rights Reserved", "url":"" },
-					"1": {"name":"Attribution-NonCommercial-ShareAlike License", "short":"CC BY-NC-SA 2.0", "url":"http://creativecommons.org/licenses/by-nc-sa/2.0/"},
-					"2": {"name":"Attribution-NonCommercial License", "short":"CC BY-NC 2.0", "url":"http://creativecommons.org/licenses/by-nc/2.0/"},
-					"3": {"name":"Attribution-NonCommercial-NoDerivs License", "short":"CC BY-NC-ND 2.0", "url":"http://creativecommons.org/licenses/by-nc-nd/2.0/"},
-					"4": {"name":"Attribution License", "short":"CC BY 2.0", "url":"http://creativecommons.org/licenses/by/2.0/"},
-					"5": {"name":"Attribution-ShareAlike License", "short":"CC BY-SA 2.0", "url":"http://creativecommons.org/licenses/by-sa/2.0/"},
-					"6": {"name":"Attribution-NoDerivs License", "short":"CC BY-ND 2.0", "url":"http://creativecommons.org/licenses/by-nd/2.0/"},
-					"7": {"name":"No known copyright restrictions", "url":"http://www.flickr.com/commons/usage/"},
-					"8": {"name":"United States Government Work", "url":"http://www.usa.gov/copyright.shtml"}
-				};
+
 				var context = {
 					beforeLoad: function() {
 						var fancyboxElement = this.element;
 						var attributionLink = fancyboxElement.attr("data-attribution-link");
 						if (!attributionLink) {
 							return;
-						} else if (attributionLink.indexOf("flickr.com") === -1) {
-							$log.error("Unable to fetch attribution data for: " + attributionLink);
-							return;
 						}
-						if (attributionLink.endsWith("/")) {
-							attributionLink = attributionLink.slice(0, -1);
-						}
-						var splitAttributionLink = attributionLink.split("/");
-						var photoId = splitAttributionLink[splitAttributionLink.length - 1];
-						if (photoId) {
-							$http({method: "GET", url: "https://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=d99915ff009f20c36657c94263e52674&photo_id=" + photoId + "&format=json&nojsoncallback=1", cache: resourceCache}).
-								success(function(data, status, headers, config) {
-									if (data.stat === "ok") {
-										var attributionDiv = $("<div class='attribution-string'></div>");
-										var authorName = data.photo.owner.realname ? data.photo.owner.realname : data.photo.owner.username;
-										var title = data.photo.title._content ? data.photo.title._content : "Untitled";
-										var nameAndTitleAnchor = $("<span><a href='" + fancyboxElement.attr("data-attribution-link") + "'>\"" + title + "\" by " + authorName + "</a></span>");
-										var separator = $("<span class='separator'>&nbsp;&nbsp;•&nbsp;&nbsp;</span>");
-										var br = $("<br>");
-										var licenseName = flickrLicenses[data.photo.license].short || flickrLicenses[data.photo.license].name;
-										var licenseAnchor = $("<span><a href='" + flickrLicenses[data.photo.license].url + "'>" + licenseName + "<a/></span><span>&nbsp;&nbsp;•&nbsp;&nbsp;</span>");
-										var modified = $("<span> Resized from original </span>");
+						attribution.getAttribution(attributionLink).
+							then(function(attributionObject) {
+								var attributionDiv = $("<div class='attribution-string'></div>");
+								var nameAndTitleAnchor = $("<span><a href='" + fancyboxElement.attr("data-attribution-link") + "'>\"" + attributionObject.title + "\" by " + attributionObject.author + "</a></span>");
+								var separator = $("<span class='separator'>&nbsp;&nbsp;•&nbsp;&nbsp;</span>");
+								var br = $("<br>");
+								var licenseAnchor = $("<span><a href='" + attributionObject.licenseUrl + "'>" + attributionObject.license + "<a/></span><span>&nbsp;&nbsp;•&nbsp;&nbsp;</span>");
+								var modified = $("<span> Resized from original </span>");
 
-										attributionDiv.append(licenseAnchor);
-										attributionDiv.append(modified);
-										attributionDiv.append(separator);
-										attributionDiv.append(br);
+								attributionDiv.append(licenseAnchor);
+								attributionDiv.append(modified);
+								attributionDiv.append(separator);
+								attributionDiv.append(br);
 
-										attributionDiv.append(nameAndTitleAnchor);
+								attributionDiv.append(nameAndTitleAnchor);
 
-										$(".fancybox-inner").append(attributionDiv);
-										$timeout(function() {
-											attributionDiv.css("opacity", "1");
-										});
-									} else {
-										$log.error(data);
-									}
-								}).
-								error(function(data, status, headers, config) {
-									$log.error(config);
+								$(".fancybox-inner").append(attributionDiv);
+								$timeout(function() {
+									attributionDiv.css("opacity", "1");
 								});
-						}
+							}, function(err) {
+								$log.error(err);
+							});
 					},
 					beforeShow: function() {
 						$(".fancybox-image").attr("alt", this.element.find("img").attr("alt"));
---
+++ client/js/services/attribution-service.js
@@ -0,0 +1,59 @@
+"use strict";
+
+angular.module("hikeio").
+	service("attribution", ["$http", "$q", "resourceCache", function($http, $q, resourceCache) {
+
+		var DEFAULT_ATTRIBUTION_TITLE = "Untitled";
+		var FLICKR_LICENSES = {
+			"0": {"name":"All Rights Reserved", "url":"" },
+			"1": {"name":"Attribution-NonCommercial-ShareAlike License", "short":"CC BY-NC-SA 2.0", "url":"http://creativecommons.org/licenses/by-nc-sa/2.0/"},
+			"2": {"name":"Attribution-NonCommercial License", "short":"CC BY-NC 2.0", "url":"http://creativecommons.org/licenses/by-nc/2.0/"},
+			"3": {"name":"Attribution-NonCommercial-NoDerivs License", "short":"CC BY-NC-ND 2.0", "url":"http://creativecommons.org/licenses/by-nc-nd/2.0/"},
+			"4": {"name":"Attribution License", "short":"CC BY 2.0", "url":"http://creativecommons.org/licenses/by/2.0/"},
+			"5": {"name":"Attribution-ShareAlike License", "short":"CC BY-SA 2.0", "url":"http://creativecommons.org/licenses/by-sa/2.0/"},
+			"6": {"name":"Attribution-NoDerivs License", "short":"CC BY-ND 2.0", "url":"http://creativecommons.org/licenses/by-nd/2.0/"},
+			"7": {"name":"No known copyright restrictions", "url":"http://www.flickr.com/commons/usage/"},
+			"8": {"name":"United States Government Work", "url":"http://www.usa.gov/copyright.shtml"}
+		};
+
+		var AttributionService = function() {
+		};
+
+		var getFlickrAttribution = function(deferred, link) {
+			if (link.endsWith("/")) {
+				link = link.slice(0, -1);
+			}
+			var splitLink = link.split("/");
+			var photoId = splitLink[splitLink.length - 1];
+			if (photoId) {
+				$http({method: "GET", url: "https://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=d99915ff009f20c36657c94263e52674&photo_id=" + photoId + "&format=json&nojsoncallback=1", cache: resourceCache}).
+					success(function(data, status, headers, config) {
+						if (data.stat === "ok") {
+							deferred.resolve({
+								author: data.photo.owner.realname ? data.photo.owner.realname : data.photo.owner.username,
+								title: data.photo.title._content ? data.photo.title._content : DEFAULT_ATTRIBUTION_TITLE,
+								license: FLICKR_LICENSES[data.photo.license].short || FLICKR_LICENSES[data.photo.license].name,
+								licenseUrl: FLICKR_LICENSES[data.photo.license].url
+							});
+						} else {
+							deferred.reject(data);
+						}
+					}).
+					error(function(data, status, headers, config) {
+						deferred.reject(config);
+					});
+			}
+		};
+
+		AttributionService.prototype.getAttribution = function(link) {
+			var deferred = $q.defer();
+			if (link.indexOf("flickr.com") !== -1) {
+				getFlickrAttribution(deferred, link);
+			} else {
+				deferred.reject("Unable to fetch attribution data for: " + link);
+			}
+			return deferred.promise;
+		};
+
+		return new AttributionService();
+	}]);
